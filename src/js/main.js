4e20bf8db8bf4cfaab1b5af0177f933d879dbb11
Anastasia <stesha1990@mail.ru>
2017-04-07 22:32:28 +0300

    circles + menu

--- src/js/main.js
+++ src/js/main.js
@@ -17,32 +17,18 @@ $(document).ready(function () {
 		bounds: 200,
 	});
 
-	// $('.js-link').on('click', function (e) {
- //        e.preventDefault();
- //        e.stopPropagation();
- //        var target = $(this).attr('href');
- //        $(target).velocity('scroll', {
- //            duration: 800,
- //            offset: -60,
- //            easing: 'ease-in-out'
- //        });
- //    });
-
- //    $('.popup-with-form').magnificPopup({
-	// 	type: 'inline',
-	// 	preloader: false,
-	// 	focus: '#name',
-	// 	callbacks: {
-	// 		beforeOpen: function() {
-	// 			if($(window).width() < 700) {
-	// 				this.st.focus = false;
-	// 			} else {
-	// 				this.st.focus = '#name';
-	// 			}
-	// 		}
-	// 	},
-
-	// });
+	$('.js-link').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var target = $(this).attr('href');
+        $(target).velocity('scroll', {
+            duration: 800,
+            offset: -60,
+            easing: 'ease-in-out'
+        });
+        $('.js-link').removeClass('active');
+        $(this).addClass('active');
+    });
 
 	$('.popup-youtube').magnificPopup({
 		disableOn: 700,
